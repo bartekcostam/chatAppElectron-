2cfc9432f65ef283da9eb3215971921574e83989
bartekcostam <bartekoffice1@gmail.com>
2020-02-05 21:21:31 +0000

    removed offline section

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,13 +1,52 @@
 <template>
-  <q-page class="flex">
-    <h5>Users page</h5>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" bordered>
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        class="q-my-sm"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-icon name="chat_bubble" color="green" />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: 'Ruddy Jedrzej',
+        },
+        {
+          id: 2,
+          name: 'Mallorie Alessandrini',
+        },
+        {
+          id: 3,
+          name: 'Elisabetta Wicklen',
+        },
+      ],
+    }
+  },
+}
 </script>
 
-
-<style>
-</style>
+<style></style>
