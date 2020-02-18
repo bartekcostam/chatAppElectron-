ed8df7f102e83551a265f0152718dea2911752bd
bartekcostam <bartekoffice1@gmail.com>
2020-02-18 20:56:50 +0000

    additing names next to messages and name on top

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -50,13 +50,15 @@
 import { Quasar, GoBack } from 'quasar'
 import { mapState, mapActions } from 'vuex'
 import { Vue } from 'vue'
+import mixinOtherUserDetails from 'src/mixins/mixin-other-user-details.js'
 export default {
+  mixins: [mixinOtherUserDetails],
   computed: {
     ...mapState('store', ['userDetails']),
     title() {
       let currentPath = this.$route.fullPath
       if (currentPath == '/') return 'Chat'
-      else if (currentPath == '/chat') return 'Chat'
+      else if (currentPath.includes('/chat')) return this.otherUserDetails.name
       else if (currentPath == '/auth') return 'Login'
     },
   },
--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="flex column">
-    <q-banner class="bg-grey-4 text-center">
-      User is offline
+    <q-banner v-if="!otherUserDetails.online" class="bg-grey-4 text-center">
+      {{ otherUserDetails.name }} is offline
     </q-banner>
     <div class="q-pa-md column col justify-end">
       <q-chat-message
         v-for="message in messages"
         :key="message.text"
-        :name="message.from"
+        :name="message.from == 'me' ? userDetails.name : otherUserDetails.name"
         :text="[message.text]"
         :sent="message.from == 'me' ? true : false"
       />
@@ -36,14 +36,16 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import mixinOtherUserDetails from 'src/mixins/mixin-other-user-details.js'
 export default {
+  mixins: [mixinOtherUserDetails],
   data() {
     return {
       newMessage: '',
     }
   },
   computed: {
-    ...mapState('store', ['messages']),
+    ...mapState('store', ['messages', 'userDetails']),
   },
   methods: {
     ...mapActions('store', [
